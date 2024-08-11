e0493abd29322b542f9aaf75e5726527a8b47875
Abhinab-karn <abhinawkarn2019@gmail.com>
2024-08-11 14:38:53 +0545

    Largest Element of An array Code

--- Array/LargestElement.java
+++ Array/LargestElement.java
@@ -1,2 +1,18 @@
 public class LargestElement {
+    int arr[] ={22,33,44,555,66,77,88};
+    int largest=arr[0];
+   public void LargElem(){
+       for (int i=1; i<arr.length; i++) {
+           if (largest<arr[i]){
+               largest=arr[i];
+           }
+       }
+       System.out.println(largest);
+
+    }
+
+    public static void main(String[] args) {
+        LargestElement le = new LargestElement();
+   le.LargElem();
+   }
 }
